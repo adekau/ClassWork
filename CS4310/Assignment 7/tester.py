102059d8c84ed4bd0b806f80ebb67c7a0f0acdd7
Alex Dekau <alex.potatofarms@gmail.com>
2016-11-09 19:48:35 -0500

    Tests.

---
+++ CS4310/Assignment 7/tester.py
@@ -0,0 +1,19 @@
+from MyVertex import MyVertex
+from MyEdge import MyEdge
+from MyGraph import MyGraph
+
+edge = MyEdge()
+vertex = MyVertex()
+graph = MyGraph()
+
+print edge.getUid()
+print vertex.getUid()
+print graph.getUid()
+
+edge = MyEdge()
+vertex = MyVertex()
+graph = MyGraph()
+
+print edge.getUid()
+print vertex.getUid()
+print graph.getUid()
